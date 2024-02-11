15d6ad6c7e8acaab2ecc71ca88c354d14c4d6e94
magaking17 <yeltaev17@gmail.com>
2024-02-11 15:01:57 +0600

    lab2

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SIS2125_Ramazan_Amanzhol"
+include(":app")
+ 
